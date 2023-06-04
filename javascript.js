eea8b24c347be443b57a49db51af2de1c4bf6100
vaishali86c <vaishali86c@gmail.com>
2023-06-04 10:12:18 +0530

    Check the results of all functions in the console log

--- javascript.js
+++ javascript.js
@@ -1,20 +1,34 @@
+
+let firstNumber = 3;
+let operator = '+';
+let secondNumber = 5;
+
+
+let result = operate(operator,firstNumber,secondNumber);
+console.log(result);
+
 function addition(a,b) {
     return a + b;
 }
+console.log(addition(3,4));
 
 function subtract(a,b) {
     return a - b;
 }
-
+console.log(subtract(3,4));
 function multiply(a,b) {
     return a * b;
 }
 
 function divide(a,b) {
-    return a / b;
+    if (b !== 0) {
+        return a/b;
+    }else {
+        return "Cannot divide by zero!";
+    }
 }
-
-function operator(operator,a,b) {
+console.log(divide(3,8));
+function operate(operator,a,b) {
 
     switch (operator) {
 
@@ -26,5 +40,7 @@ function operator(operator,a,b) {
             return multiply(a,b)
         case '/':
             return divide(a,b);
+        default:
+            return "Invalid operator!";
     }
 }
